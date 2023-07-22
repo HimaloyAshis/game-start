34dfd028eed3fe10274e53cdc400ef079b075ab9
himaloyash <himaloyashis98@gmail.com>
2023-07-22 22:45:56 +0600

    error element route added

--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
-            
+            <Link>Home</Link>
+            <Link>College</Link>
+            <Link>Admission</Link>
+            <Link>My College</Link>
         </div>
     );
 };
