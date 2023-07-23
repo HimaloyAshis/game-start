1a02021443281b84e42c546dd7c54d61430d72ce
himaloyash <himaloyashis98@gmail.com>
2023-07-24 01:12:25 +0600

    queryclient added

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes.jsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <div className='bg-[#F5F5F5] '>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}>
+      </RouterProvider>
+    </QueryClientProvider>
   </div>
   // {/* </React.StrictMode>, */}
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Home = () => {
+
+    
+
     return (
         <>
             <section>
@@ -15,7 +18,16 @@ const Home = () => {
             {/* 3 card */}
 
             <section>
-                
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
             </section>
 
         </>
