0cea4f928657dfe9db4100c8f53f1e0b547ccd3e
himaloyash <himaloyashis98@gmail.com>
2023-07-23 22:47:39 +0600

    login set up

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,7 +4,32 @@ import './Login.css'
 const Login = () => {
     return (
         <div className='anima'>
-            <h1>Login</h1>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <h1 className="text-5xl font-bold">Login now!</h1>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
