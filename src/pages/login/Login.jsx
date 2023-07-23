c62338f1ae77762c9e90ff773e756fb17d2290b5
himaloyash <himaloyashis98@gmail.com>
2023-07-23 23:14:14 +0600

    social media login ok

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,6 +7,7 @@ const Login = () => {
             <div className="hero min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
+                        <img src="https://img.freepik.com/free-vector/tablet-login-concept-illustration_114360-7863.jpg?size=626&ext=jpg&ga=GA1.2.1842592228.1683275805&semt=ais" alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
