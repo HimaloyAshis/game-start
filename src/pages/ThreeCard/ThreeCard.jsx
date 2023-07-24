38dc0b017c06b7b89578769d492547bb2c4446d5
himaloyash <himaloyashis98@gmail.com>
2023-07-24 12:43:53 +0600

    data formation done

--- src/pages/ThreeCard/ThreeCard.jsx
+++ src/pages/ThreeCard/ThreeCard.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 
-const ThreeCard = ({card}) => {
+const ThreeCard = ({ card }) => {
 
-    const {_id, admission_date,college_image, college_name, event, number_of_research, rating,research_history,sports} = card
-    console.log(card)
+    const { _id, admission_date, college_image, college_name, event, number_of_research, rating, research_history, sports } = card
+    // console.log(card)
 
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <figure><img src={college_image} alt="colleges" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{college_name}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title font-bold">College:{college_name}</h2>
+                    <p className='font-semibold'>admission_date: {admission_date}</p>
+                    <p className='font-semibold'>Event: {event}</p>
+                    <div className='mb-3'><span className='text-xl font-bold'>Research History</span> {
+                        research_history.map((research, index) =>
+                            <>
+                                <div key={index}>
+
+                                    <p>.{index + 1}</p>
+                                    <p className='font-semibold'>Faculty: {research.faculty}</p>
+                                    <p className='font-semibold'>Topic: {research.topic}</p>
+                                    <p className='font-semibold'>Year: {research.year}</p>
+
+                                </div>
+
+                            </>
+
+                        )
+
+
+                    }
+                        < div >
+                            <p className='font-bold mt-3'>Sports</p>
+                            {
+                                sports.map((sport, index) => <p key={index}>{index + 1}. {sport} </p>)
+                            }
+                        </div>
+                    </div>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Know more</button>
+                        <button className="btn btn-primary ">Know more</button>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
